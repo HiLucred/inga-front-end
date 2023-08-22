975aeb9ade40956ae437e7457a0a9db728f11202
Luc <lucred@protonmail.com>
2023-08-22 11:51:04 -0300

    style: Text shadow add in title and subtitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,24 @@ import { Button } from '@/components/Button'
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center mt-20">
-      <div className="flex items-center justify-center mb-20">
-        <h1 className="text-8xl md:text-9xl font-extrabold bg-gradient-to-tr from-blue-600 to-red-600 text-transparent bg-clip-text">
+      <div
+        className="flex items-center justify-center mb-20"
+        style={{
+          textShadow: '0 4px 102px #ff000055',
+        }}
+      >
+        <h1 className="text-8xl sm:text-9xl font-extrabold bg-gradient-to-tr from-blue-600 to-red-600 text-transparent bg-clip-text">
           ING
         </h1>
         <Image src={triangle} alt="" width={108} />
       </div>
 
-      <div className="flex flex-col items-center mb-16">
+      <div
+        className="flex flex-col items-center mb-16"
+        style={{
+          textShadow: '0 4px 120px #1852e586',
+        }}
+      >
         <h1 className="text-white text-4xl font-extrabold leading-none text-center sm:text-6xl 2xl:text-7xl">
           Sua
           <span className="bg-gradient-to-tr from-purple-600 to-red-500 text-transparent bg-clip-text">
@@ -22,9 +32,9 @@ export default function Home() {
         </h1>
 
         <h2 className="mt-4 text-lg font-bold text-white text-center sm:text-4xl 2xl:text-5xl ">
-          Feito <span className="underline">por</span> artistas independentes.
+          feito <span className="underline">por</span> artistas independentes
           <br />
-          <span className="underline">Para</span> artistas independentes.
+          <span className="underline">para</span> artistas independentes.
         </h2>
       </div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
       colors: {
         'my-purple': '#6829B8',
         'my-red': '#FF4545',
